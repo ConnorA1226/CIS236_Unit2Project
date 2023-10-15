81a017f9008d01e87561c256c6edd47c46edf180
Connor Arends <connor.arends@southeasttech.edu>
2023-10-15 14:40:03 -0500

    Added a few things Still have a few errors

---
+++ CIS236_Unit2Project/Controllers/ItemController.cs
@@ -0,0 +1,19 @@
+﻿using CIS236_Unit2Project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS236_Unit2Project.Controllers;
+public class ItemController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ItemController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var items = _context.Items.ToList();
+        return View(items);
+    }
+}
---
+++ CIS236_Unit2Project/Models/DbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace CIS236_Unit2Project.Models;
+using Microsoft.EntityFrameworkCore;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Item> Items { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Item>().HasData(
+            new Item { Id = 1, Name = "Item 1", Price = 9.99 },
+            new Item { Id = 2, Name = "Item 2", Price = 19.99 }
+        );
+    }
+}
